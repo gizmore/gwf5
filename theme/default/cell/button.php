8c3c52e13f344c549b34b9e6a26e84362d671882
gizmore <gizmore@wechall.net>
2017-06-13 19:20:09 +0200

    Admin section button only shown when available

--- theme/default/cell/button.php
+++ theme/default/cell/button.php
@@ -1,2 +1,4 @@
 <?php $field instanceof GDO_Button; ?>
+<?php if ($href) : ?>
 <a href="<?php echo $href; ?>" class="md-button"><?php echo $field->htmlIcon(); ?><?php echo $field->displayLabel(); ?></a>
+<?php endif; ?>
