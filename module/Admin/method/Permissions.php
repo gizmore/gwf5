cf5e0d50ee9d95bb3427ce59757f7ff1b0e63915
gizmore <gizmore@wechall.net>
2017-06-25 14:45:15 +0200

    sync

--- module/Admin/method/Permissions.php
+++ module/Admin/method/Permissions.php
@@ -23,20 +23,15 @@ class Admin_Permissions extends GWF_MethodQueryTable
 		);
 	}
 	
+	public function getQuery()
+	{
+		return $this->getGDO()->select('perm_id, perm_name')>select('COUNT(perm_user_id) user_count')->
+		join('LEFT JOIN gwf_userpermission ON perm_id = perm_perm_id')->group('perm_id,perm_name');
+	}
+	
 	public function execute()
 	{
 		return $this->renderNavBar()->add(parent::execute());
-	}
-	
-	public function getResult()
-	{
-		return $this->filterQuery(GWF_Permission::table()->select('perm_id, perm_name')->select('COUNT(perm_user_id) user_count')->
-		join('LEFT JOIN gwf_userpermission ON perm_id = perm_perm_id')->group('perm_id,perm_name'))->exec();
-	}
-	
-	public function getResultCount()
-	{
-		return GWF_Permission::table()->countWhere('1');
 	}
 	
 }
