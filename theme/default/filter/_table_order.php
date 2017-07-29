02efc24e58f64e68ce5f7cd0b3929d1fd73e4297
gizmore <gizmore@wechall.net>
2017-07-29 03:27:41 +0200

    tablesorting-css

--- theme/default/filter/_table_order.php
+++ theme/default/filter/_table_order.php
@@ -1,6 +1,5 @@
 <?php $field instanceof GDOType; ?>
 <div class="gwf-tblorder">
-  <a href="<?= $url_asc; ?>" class="asc<?= $is_asc ? ' sel' : ''; ?>">^</a>
-  <br/>
-  <a href="<?= $url_desc; ?>" class="desc<?= $is_desc ? ' sel' : ''; ?>">v</a>
+  <a href="<?= $url_asc; ?>" class="asc<?= $is_asc ? ' sel' : ''; ?>"><i class="material-icons">arrow_drop_up</i></a>
+  <a href="<?= $url_desc; ?>" class="desc<?= $is_desc ? ' sel' : ''; ?>"><i class="material-icons">arrow_drop_down</i></a>
 </div>
